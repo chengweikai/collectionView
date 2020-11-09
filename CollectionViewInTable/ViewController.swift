a2a23fe939ee8ef1c2b854d6227b81edfe938c9f
cwk <cwk1991s@gmail.com>
2020-11-10 07:08:56 +0800

    Initial Commit

---
+++ CollectionViewInTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewInTable
+//
+//  Created by Vic on 2020/11/10.
+//  Copyright © 2020 codeWalker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
